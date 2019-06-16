3f48148bc3f3ab74d178a0a104d4d1fc2cb02d9e
danila puzikov <fiend.of.sound@gmail.com>
2019-06-16 22:06:49 +0300

    chore: cleaner data

--- stories/index.stories.jsx
+++ stories/index.stories.jsx
@@ -21,10 +21,6 @@ const users = [
     id: 14,
     englishName: 'Hello World',
     englishPosition: 'Adam West',
-    russianName: 'Hello World',
-    russianPosition: 'Adam West',
-    tamtam: null,
-    email: null,
     phone: '11111111111111',
     avatarUrl: '',
     isMember: true
@@ -33,23 +29,15 @@ const users = [
     id: 13,
     englishName: 'new1',
     englishPosition: 'new11',
-    russianName: 'new1',
-    russianPosition: 'new1',
-    tamtam: null,
-    email: null,
     phone: '22223333333333',
     avatarUrl:
       'http://localhost:3000/api/images/avatar-prefix-n01-f3b64fc0-8dd6-11e9-8742-e1c0a227e48e/f3b64fc0-8dd6-11e9-8742-e1c0a227e48e-2019-04-25%2023.33.10.jpg',
-    isMember: true
+    isMember: false
   },
   {
     id: 12,
     englishName: 'Vannessa',
     englishPosition: 'newm',
-    russianName: 'newm',
-    russianPosition: 'newm',
-    tamtam: null,
-    email: null,
     phone: '22222222222',
     avatarUrl:
       'http://localhost:3000/api/images/avatar-prefix-n01-2cb6da20-8dc7-11e9-8fb6-410410f818a4/2cb6da20-8dc7-11e9-8fb6-410410f818a4-Favicon%20bapp.png',
